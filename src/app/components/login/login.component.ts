165be5c5f36d66c990b1361b8fc1c59d947a0c64
Abdulfattah Hessein <76224774+AbdulfattahHessein@users.noreply.github.com>
2023-09-30 14:41:54 +0300

    last one

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,3 @@
-import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
@@ -9,11 +8,7 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor(
-    private router: Router,
-    private authService: AuthService,
-    private location: Location
-  ) {}
+  constructor(private router: Router, private authService: AuthService) {}
   login() {
     this.authService.login('username', 'password');
 
